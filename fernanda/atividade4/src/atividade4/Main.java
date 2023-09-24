a89952e4082977c535a0b2cb7728f946c225e858
Gabrielle <gabriellefelixferr@gmail.com>
2023-09-24 19:52:22 -0300

    adicionando nota de aula 4 da aula 6

---
+++ fernanda/atividade4/src/atividade4/Main.java
@@ -0,0 +1,11 @@
+package atividade4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro("Toyota", "Corolla");
+        carro.mostrarDetalhes();
+    }
+
+	}
